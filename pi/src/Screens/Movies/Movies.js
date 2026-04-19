cb28de20eeb7449751ab82cc21465e454fa5ed19
matiasblousson <mblousson@udesa.edu.ar>
2026-04-19 00:41:39 -0300

    Busqueda pelis y serie

--- pi/src/Screens/Movies/Movies.js
+++ pi/src/Screens/Movies/Movies.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import CardMovie from '../../components/CardMovie/CardMovie';
-import './styles.css';
+import './Movie.css';
 
 class Peliculas extends Component {
     constructor(props) {
         super(props);
         this.state = {
             peliculas: [],
-            filtro: ''
+            filtro: '',
+            valor: '',
+            cantidad: 8
         }
         this.paginaActual = 1;
     }
@@ -17,54 +19,59 @@ class Peliculas extends Component {
         .then(respuesta => respuesta.json())
         .then(datos => this.setState({ peliculas: datos.results }))
         .catch(error => console.error(error));
+        
     }
 
 
-    controlarCambios(evento) {
-        this.setState({ filtro: evento.target.value });
-    }
-
-enviarFormulario(event) {
+evitarSubmit(event) {
     event.preventDefault();
+  }
 
-    if (this.state.filtro != "") {
-        this.props.history.push("/resultadosPelicula/" + this.state.filtro)
+controlarCambios(event) {
+    this.setState({valor: event.target.value});
+  }
+
+filtrarPersonajes(){
+    if(this.state.peliculas == undefined){
+        return []
     }
+    let personajeBuscado = this.state.peliculas.filter((item) => {
+        return item.title && item.title.toLowerCase().includes(this.state.valor.toLowerCase())
+    })
+    return personajeBuscado
 }
     
-
-    cargarMas() {
-        this.paginaActual = this.paginaActual + 1;
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=ed64b41cac1f7454df1403e56e96ce49&page=${this.paginaActual}`)
-        .then(respuesta => respuesta.json())
-        .then(datos => this.setState({ peliculas: this.state.peliculas.concat(datos.results) }))
-        .catch(error => console.error(error));
-    }
+cargarMas() {
+    this.paginaActual = this.paginaActual + 1;
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=ed64b41cac1f7454df1403e56e96ce49&page=${this.paginaActual}`)
+    .then(respuesta => respuesta.json())
+    .then(datos => this.setState({
+        peliculas: this.state.peliculas.concat(datos.results),
+        cantidad: this.state.cantidad + 8
+    }))
+    .catch(error => console.error(error));
+}
 
     render() {
-        let peliculasFiltradas = this.state.peliculas;
+        let peliculasFiltradas = this.filtrarPersonajes()
+        
         return (
             <div className='container'>
-                <div className='barra-busqueda-peliculas'>
-                    <form className='form-busqueda-peliculas' onSubmit={(event)=>this.enviarFormulario(event)}>
-                        <input type="text" placeholder="Buscar películas" onChange={(event)=>this.controlarCambios(event)} value={this.state.filtro} />
-                        <input type="submit" value="Buscar" />
-                    </form>
-                </div>
-                <br/>
-                <h2 className='TituloMovie'>Todas las películas</h2>
-                <div className="contenedor-boton-cargar">
-                    <button className='boton-cargar-mas' onClick={() => this.cargarMas()}>
-                        Cargar más
-                    </button>
+                <div className='encabezado-movies'>
+                    <div className='barra-busqueda-movies'>
+                        <form className='form-busqueda-movies' onSubmit={(event)=>this.evitarSubmit(event)}>
+                            <input className='input-busqueda-movies' type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} placeholder='Nombre a buscar'/>
+                        </form>
+                    </div>
+                    <h2 className='TituloMovie'>Todas las películas</h2>
                 </div>
                 
                 <section className='row cards all-movies' id='movies'>
                     {peliculasFiltradas.length > 0 ? (
-                        peliculasFiltradas.map((pelicula, indice) => {
+                        peliculasFiltradas.slice(0, this.state.cantidad).map((pelicula, indice) => {
                             return (
                                 <CardMovie
-                                    key={indice}
+                                    key={pelicula.id}
                                     data={pelicula}
                                     
                                 />
@@ -74,6 +81,11 @@ enviarFormulario(event) {
                         <p><img src={`/img/Gif.gif`} alt="..Cargando"></img></p>
                     )}
                 </section>
+                <div className="contenedor-boton-cargar">
+                    <button className='boton-cargar-mas' onClick={() => this.cargarMas()}>
+                        Cargar más
+                    </button>
+                </div>
             </div>
         )
     }
--- pi/src/Screens/Series/Series.js
+++ pi/src/Screens/Series/Series.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CardSerie from '../../components/CardSerie/CardSerie';
-import './Styles.css';
+import './Series.css';
 
 
 class Series extends Component {
@@ -8,7 +8,9 @@ class Series extends Component {
         super(props);
         this.state = {
             series: [],
-            filtro: ''
+            filtro: '',
+            valor: "",
+            cantidad: 8
         }
         this.paginaActual = 1;
     }
@@ -20,58 +22,70 @@ class Series extends Component {
         .catch(error => console.error(error));
     }
 
-    evitarSubmit(evento) {
-        evento.preventDefault();
-    }
-
-    controlarCambios(evento) {
-        this.setState({ filtro: evento.target.value });
-    }
-
-enviarFormulario(event) {
+evitarSubmit(event) {
     event.preventDefault();
-    if (this.state.filtro != "") {
-        this.props.history.push("/resultadosSeries/" + this.state.filtro)
-    }
-}
+  }
 
-    cargarMas() {
+controlarCambios(event) {
+    this.setState({valor: event.target.value});
+  }
+
+filtrarPersonajes(){
+    if(this.state.series == undefined){
+        return []
+    }
+    let personajeBuscado = this.state.series.filter((item) => {
+        return item.name && item.name.toLowerCase().includes(this.state.valor.toLowerCase())
+    })
+    return personajeBuscado
+}
+    
+cargarMas() {
         this.paginaActual = this.paginaActual + 1;
         fetch(`https://api.themoviedb.org/3/tv/airing_today?api_key=ed64b41cac1f7454df1403e56e96ce49&page=${this.paginaActual}`)
         .then(respuesta => respuesta.json())
-        .then(datos => this.setState({ series: this.state.series.concat(datos.results) }))
+        .then(datos => this.setState({ series: 
+            this.state.series.concat(datos.results),
+            cantidad: this.state.cantidad + 8 }))
         .catch(error => console.error(error));
-    }
+}
 
     render() {
-        let seriesFiltradas = this.state.series;
-
+        let seriesFiltradas = this.filtrarPersonajes()
+        console.log(seriesFiltradas);
+        
         return (
             <div className='container'>
-                <div className='barra-busqueda-peliculas'>
-                    <form className='form-busqueda-peliculas' onSubmit={(event)=>this.enviarFormulario(event)}>
-                        <input type="text" placeholder="Buscar películas" onChange={(event)=>this.controlarCambios(event)} value={this.state.filtro} />
-                        <input type="submit" value="Buscar" />
-                    </form>
+                <div className='encabezado-series'>
+                    <div className='barra-busqueda-series'>
+                        <form className='form-busqueda-movies' onSubmit={(event)=>this.evitarSubmit(event)}>
+                            <input className='input-busqueda-movies' type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} placeholder='Nombre a buscar'/>
+                        </form>
+                    </div>
+                    <h2 className='TituloSeries'>Todas las Series</h2>
                 </div>
-                <h2 className='TituloSerie'>Todas las series</h2>
-                <br/>
-                <div className="contenedor-boton-cargar">
+                
+
+                <section className='row cards all-series' id='series'>
+                    {seriesFiltradas.length > 0 ? (
+                        seriesFiltradas.slice(0, this.state.cantidad).map((series, indice) => {
+                            return (
+                                <CardSerie
+                                    key={series.id}
+                                    data={series}
+                                    
+                                />
+                            )
+                        })
+                    ) : (
+                        <p><img src={`/img/Gif.gif`} alt="..Cargando"></img></p>
+                    )}
+                </section>
+                                <div className="contenedor-boton-cargar">
                     <button className='boton-cargar-mas' onClick={() => this.cargarMas()}>
                         Cargar más
                     </button>
                 </div>
-
-                <section className='row cards all-series' id='series'>
-                    {seriesFiltradas.map((serie, indice) => {
-                        return (
-                            <CardSerie
-                                key={indice}
-                                data={serie}
-                            />
-                        )
-                    })}
-                </section>
             </div>
         )
     }
